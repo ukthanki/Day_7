4fb55d750b391b7502ec0079d6564cfd96fa1a5c
Umang Thanki <u_thanki@yahoo.com>
2022-01-21 21:17:07 -0600

    Final Project Commit

--- hangman.py
+++ hangman.py
@@ -1,32 +1,37 @@
 import random
+import hangman_words
+import hangman_art
 
-word_list = ["ardvark", "baboon", "camel", "lion","jaguar"]
-
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 display = []
 
 for letter in chosen_word:
     display.append("_")
-print(chosen_word)
-print(display)
+
+print(hangman_art.logo)
+
+print(" ".join(display))
 
 lives = 6
 
 while lives > 0:
     # Guessed Letter input
-    guessed_letter = input("Guess a letter: ").lower()
+    guessed_letter = input("\nGuess a letter: ").lower()
     letter_mismatch = 0
     # Checking if the letter is in the word
     for i in range(0, word_length):
         if chosen_word[i] == guessed_letter:
+            if display[i] == guessed_letter:
+                print(f"You have already guessed '{guessed_letter}.'")
             display[i] = guessed_letter
         else:
             letter_mismatch += 1
             if letter_mismatch == word_length:
+                print(f"You guessed '{guessed_letter}', but it is not in the word. You lose a life.")
                 lives -= 1
                
-    print(f"Lives left: {lives}")
+    print(hangman_art.stages[lives])
 
     if lives == 0:
         print("YOU LOST")
@@ -35,8 +40,6 @@ while lives > 0:
         lives = 0
         print("YOU WON")
 
-    print(display)
-    
-    # print("".join(display))
+    print(" ".join(display))
 
 
